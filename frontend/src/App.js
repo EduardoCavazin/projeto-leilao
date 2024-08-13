82529f2bf2194dc5f4c0d60d6031c0f06c326e25
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-08-12 21:30:23 -0300

    Login page completed and Forgot Password page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import DefaultLayout from './components/DefaultLayout';
 import SimpleLayout from './components/SimpleLayout';
+import ForgotPassword from './pages/forgotPassword/ForgotPassword';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DefaultLayout><Home/></DefaultLayout>} />
           <Route path='/login' element={<SimpleLayout><Login/></SimpleLayout>} />
+          <Route path='/forgot-password' element={<SimpleLayout><ForgotPassword/></SimpleLayout>} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Login.css";
+import { Link } from "react-router-dom";
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+
 
 const Login = () => {
-    return(
+    const buttons = (
+        <>
+            <Button label="Entrar"/>
+            <Button label="Cadastrar" severity='help' style={{ marginLeft: '0.5em' }}/>
+        </>
+    )
+
+    return (
         <div className="login-container">
             <h1>Login</h1>
             <Link to="/">Home</Link>
+            <Card title="Login" footer={buttons} className=" flex flex-colum">
+                <InputText placeholder="Usuário" />
+                <Password  toggleMask/>
+                <Link to="/forgot-password" className="forgot-password-link">
+                    Esqueceu a senha?
+                </Link>
+            </Card>
         </div>
     );
 }
