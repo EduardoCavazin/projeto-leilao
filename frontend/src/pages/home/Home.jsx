9f41445dcce9f1cece6c7feb57ca31413c93fc71
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-08-19 20:36:15 -0300

    Criação de botão "Logout" na tela inicial

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { Button } from 'primereact/button';
 
 const Home = () => {
+    const logout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('usuario');
+        window.location.href = '/login';
+    }
+    
     return (
         <div className="home-container">
             <h1>Bem Vindo Amiguinho</h1>
             <p>Você está na HomePage</p>
             <Link to="/login">Login</Link>
+
+            <Button label='Sair' onClick={logout}/>
         </div>
     );
 };
