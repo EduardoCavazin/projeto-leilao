ed4834261578e1aa6c8cc89c0378293066940399
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-08-26 20:11:52 -0300

    Correção da cagada

--- README.md
+++
@@ -1,6 +0,0 @@
-# Projeto de Leilão
-
-Projeto desenvolvido para as aulas de Programação Web
-no 3º ano de Engenharia de Software do IFPR-Paranavaí
-
-Aluno: Eduardo Tormena Cavazin
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
+import { useTranslation } from 'react-i18next';
 
 const Home = () => {
-    
+    const {t} = useTranslation
+
     return (
         <div className="home-container">
-            <h1>Bem Vindo Amiguinho</h1>
+            <h1>{t('welcome')}Bem Vindo Amiguinho</h1>
             <p>Você está na HomePage</p>
         </div>
     );
