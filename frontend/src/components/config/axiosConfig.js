e3d975c43df620065bc8265ebc6fb2be3094bc07
Eduardo Cavazin <eduardotorcav@gmail.com>
2024-12-02 22:02:18 -0300

    Reset

--- frontend/src/components/config/axiosConfig.js
+++ frontend/src/components/config/axiosConfig.js
@@ -8,4 +8,17 @@ const api = axios.create({
 
 });
 
+api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        console.log(token);
+        if(token){
+            console.log('entrou');
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => Promise.reject(error)
+);
+
 export default api;
